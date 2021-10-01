ddcc9b7324efb267c4e637bb9e0021bc7738c936
callmevolk <callmevolk@gmail.com>
2021-10-01 19:03:31 +0200

    SEO fixes

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
 
-const SEO = ({ title, description }) => {
+const SEO = ({ title, description, image_url }) => {
   const {site} = useStaticQuery(graphql`
   {
     site {
@@ -18,6 +18,11 @@ const SEO = ({ title, description }) => {
     <Helmet>
       <title>{`${title} | ${siteTitle}`}</title>
       <meta name="description" content={description || siteDesc} />
+      <meta property="og:title" content={`${title} | ${siteTitle}`}/>
+      <meta property="og:description" content={description}/>
+      <meta property="og:image" itemProp="image" content={image_url !== undefined ? image_url : `../assets/images/slider/slika-1.png`}/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content={"https://res-publica.netlify.app/"}/>
     </Helmet>
   )
 }
