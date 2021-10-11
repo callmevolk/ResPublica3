3988cc43777c8deb801d8211bdd76c99b5c54f2b
callmevolk <callmevolk@gmail.com>
2021-10-11 13:29:22 +0200

    changed contentful profile

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `5cktfvfmetn9`,
+        spaceId: `qbmgqmkci02u`,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_API_KEY,
       },
